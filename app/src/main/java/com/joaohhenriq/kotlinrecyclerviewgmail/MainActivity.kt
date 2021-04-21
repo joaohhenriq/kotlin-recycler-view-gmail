c63261534b5c38326ca7e840e2635950aec68f9a
joaohhenriq <joao_camargo10@hotmail.com>
2021-04-20 21:37:25 -0300

    Working with fakeit

--- app/src/main/java/com/joaohhenriq/kotlinrecyclerviewgmail/MainActivity.kt
+++ app/src/main/java/com/joaohhenriq/kotlinrecyclerviewgmail/MainActivity.kt
@@ -4,15 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.joaohhenriq.kotlinrecyclerviewgmail.adpater.EmailAdapter
+import com.joaohhenriq.kotlinrecyclerviewgmail.model.email
 import com.joaohhenriq.kotlinrecyclerviewgmail.model.fakeEmails
+import com.mooveit.library.Fakeit
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: EmailAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Fakeit.init()
+
         setContentView(R.layout.activity_main)
 
-        recycler_view_main.adapter = EmailAdapter(fakeEmails())
+        adapter = EmailAdapter(fakeEmails())
+        recycler_view_main.adapter = adapter
         recycler_view_main.layoutManager = LinearLayoutManager(this)
+
+        floating_button.setOnClickListener {
+            addEmail()
+            recycler_view_main.scrollToPosition(0)
+        }
+    }
+
+    private fun addEmail() {
+        val sdf = SimpleDateFormat("dd/MM/YYYY", Locale("pt", "BR")).parse(
+            Fakeit.dateTime().dateFormatter()
+        )
+
+        adapter.emails.add(0, email {
+            stared = false
+            unread = true
+            user = Fakeit.name().firstName()
+            subject = Fakeit.company().name()
+            date = SimpleDateFormat("d MMM", Locale("pt", "BR")).format(sdf)
+            preview = mutableListOf<String>().apply {
+                repeat(10) {
+                    val newWord: String = Fakeit.lorem().words()
+                    add(newWord)
+                }
+            }.joinToString( " " )
+        })
+
+        adapter.notifyItemInserted(0)
     }
 }
